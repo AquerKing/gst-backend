b07da1363f5f0127b926a5e4ceb8e9fccedbf152
AquerKing <aquerking@163.com>
2026-09-01 13:28:31 +0800

    Create Spring Boot project

---
+++ src/main/java/cn/grassit/grassitbackend/GrassItBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.grassit.grassitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrassItBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/grassit/grassitbackend/GrassItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.grassit.grassitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
